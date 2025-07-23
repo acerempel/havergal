f6ca56f6916ce06cdb56c5dd7e13c2b6ac7f1268
Alan Rempel <alan.rempel@gmail.com>
2025-07-23 19:01:43 -0400

    misc

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,43 @@ function App() {
 
   const [parseErrors, setParseErrors] = createSignal([]);
 
-  function importTransactions(ev) {
+  function submitForm(ev) {
     ev.preventDefault();
-    const t_tra = grist.getTable("Transactions");
-    const t_imp = grist.getTable("Imported");
     Papa.parse(file_input.files[0], {
       header: true,
-      complete: (results, file) => {
-        if (results.errors.length > 0) {
-          setParseErrors(results.errors);
-          return;
-        }
-        t_imp.create(results.data)
+      skipEmptyLines: true,
+      error: (er) => {
+        console.error(`Unable to read file: ${er}`)
+      },
+      complete: (results, _file) => {
+        importTransactions(results)
       }
     })
   }
 
+  function importTransactions(results) {
+    if (results.errors.length > 0) {
+      setParseErrors(results.errors);
+      return;
+    }
+    const t_tra = grist.getTable("Transactions");
+    const t_imp = grist.getTable("Imported");
+    const rules = grist.docApi.fetchTable("Rules");
+    const srule = [];
+    for (const col in rules) {
+      rules[col].forEach((val, ix) => {
+        if (srule[ix] == undefined) {
+          srule[ix] = {}
+        }
+        srule[ix][col] = val;
+      })
+    }
+    t_imp.create(results.data)
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={submitForm}>
         <label>
           Choose a CSV file containing transactions to import.
           <input ref={file_input} type="file" name="transactions" accept="text/csv" />
@@ -37,7 +55,7 @@ function App() {
         <p>The following errors occurred:</p>
         <ul>
           <For each={parseErrors()}>
-            {(er) => <li>{er.message}</li>}
+            {(er) => <li>Row {er.row}: {er.message}</li>}
           </For>
         </ul>
       </Show>
